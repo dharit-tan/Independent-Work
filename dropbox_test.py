593886a0685797e7a091a81ee2dc9bbd0b8e90ad
Reed <tortorareed@gmail.com>
2014-02-19 12:17:50 -0500

    asdf

--- dropbox_test.py
+++ dropbox_test.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
 import requests
+import parrot_fs_dropbox
+import dropbox
 
 dropbox_authorize_url = 'https://www.dropbox.com/1/oauth2/authorize'
 dropbox_login_url = 'https://www.dropbox.com/login'
 
 # Include the Dropbox SDK libraries
-from dropbox import client, rest, session
+# from dropbox import client, rest, session
 
 # Get your app key and secret from the Dropbox developer website
 APP_KEY = 'vu3xexqedjpw523'
@@ -14,36 +16,23 @@ APP_SECRET = '8fyq6q9qmb0wn6l'
 
 # ACCESS_TYPE should be 'dropbox' or 'app_folder' as configured for your app
 ACCESS_TYPE = 'app_folder'
+flow = dropbox.client.DropboxOAuth2FlowNoRedirect(APP_KEY, APP_SECRET)
 
-sess = session.DropboxSession(APP_KEY, APP_SECRET, ACCESS_TYPE)
-request_token = sess.obtain_request_token()
-url = sess.build_authorize_url(request_token)
+authorize_url = flow.start()
+print '1. Go to: ' + authorize_url
+print '2. Click "Allow" (you might have to log in first)'
+print '3. Copy the authorization code.'
+code = raw_input("Enter the authorization code here: ").strip()
 
-# r = requests.get(url)
-
-# print r.text
-# print r.url
-
-# payload = {'login_email':'dtantivi@princeton.edu','login_password':'dharit1250','cont':'https://www.dropbox.com/1/oauth2/authorize?response_type=code&client_id=vu3xexqedjpw523'}
-# payload = {'t':'t:_JNQtDOKSqcTa5PGYzaBc05W','login_email':'dtantivi@princeton.edu','login_password':'dharit1250','cont':'https://www.dropbox.com/1/oauth2/authorize?response_type=code&client_id=vu3xexqedjpw523','signup_tag':'oauth','signup_data':'534819','display':'desktop','login_submit':'1','remember_me':'off','login_submit_dummy':'Sign in'}
-# payload = {'login_email':'dtantivi@princeton.edu','login_password':'dharit1250','remember_me':'off'}
-# r = requests.post(r.url, data=payload)
-
-# print r.text
-
-# print r.text
-# print r.data
-
-# Make the user sign in and authorize this token
-print "url:", url
-print "Please visit this website and press the 'Allow' button, then hit 'Enter' here."
-raw_input()
 
 # This will fail if the user didn't visit the above URL
-access_token = sess.obtain_access_token(request_token)
-
+# access_token = sess.obtain_access_token(request_token)
+access_token, user_id = flow.finish(code)
 # Print the token for future reference
 print access_token
+
+client = dropbox.client.DropboxClient(access_token)
+print 'linked account: ', client.account_info()
 
 
 
--- parrot_fs_dropbox.py
+++ parrot_fs_dropbox.py
@@ -1,2 +1,64 @@
+import os
 
-class 
+dropbox_authorize_url = 'https://www.dropbox.com/1/oauth2/authorize'
+DROPBOX_DIR = "~/parrot_fs_dropbox_private/"
+
+# Get your app key and secret from the Dropbox developer website
+APP_KEY = 'vu3xexqedjpw523'
+APP_SECRET = '8fyq6q9qmb0wn6l'
+
+# ACCESS_TYPE should be 'dropbox' or 'app_folder' as configured for your app
+ACCESS_TYPE = 'app_folder'
+
+RD_ONLY = 0
+WR_ONLY = 1
+RD_WR = 2
+
+fd_table = [0 for x in range(10)]
+
+class pfs_file_dropbox:
+	def __init__(self, name, f):
+		self.name = name
+		self.f = f
+
+class pfs_service_dropbox:
+	def __init__(self):
+		self.authorize()
+
+	def authorize(self):
+		flow = dropbox.client.DropboxOAuth2FlowNoRedirect(APP_KEY, APP_SECRET)
+
+		authorize_url = flow.start()
+		print '1. Go to: ' + authorize_url
+		print '2. Click "Allow" (you might have to log in first)'
+		print '3. Copy the authorization code.'
+		code = raw_input("Enter the authorization code here: ").strip()
+
+		# This will fail if the user didn't visit the above URL
+		access_token, user_id = flow.finish(code)
+
+		# Print the token for future reference
+		print access_token
+
+		self.client = dropbox.client.DropboxClient(access_token)
+		print 'linked account: ' + client.account_info()
+
+	def open(self, name, flags):
+		filename = name.split('/')[-1]
+
+		current = os.getcwd()
+		os.chdir(DROPBOX_DIR)
+		self.client.getfile(name)
+
+		if flags & RD_ONLY:
+			fo = open(name, "r")
+		elif flags & WR_ONLY:
+			fo = open(name, "w")
+		elif flags & RD_WR:
+			fo = open(name, "w+")
+
+		os.chdir(current)
+
+
+	def close(self, name):
+		
