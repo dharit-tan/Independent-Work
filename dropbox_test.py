a0d5bce3a07d0f61de5978173033433b1da6f7c0
Reed <tortorareed@gmail.com>
2014-03-06 03:42:58 -0500

    open working somewhat

--- dropbox_test.py
+++ dropbox_test.py
@@ -1,12 +1,35 @@
 #!/usr/bin/env python
 
-import requests
+# import requests
+import os
 from parrot_fs_dropbox import pfs_service_dropbox, pfs_file_dropbox
 
+def test1(service):
+	print
+	print "# TEST 1 ------------------------------------#"
+	print "# Testing open with file in root dir. Should see contents of chop.py"
+	f = service.open("chop.py", "r+")
+	for i in f.readlines():
+		print i
+	service.close("chop.py")
+	
+def test2(service):
+	print
+	print "# TEST 2 ------------------------------------#"
+	print "# Testing chdir (forward and backwards) and finally cd back to HOME"
+	service.chdir("HALL MUSIC")
+	print os.getcwd()
+	service.chdir(".")
+	print os.getcwd()
+	service.chdir("..")
+	print os.getcwd()
+	service.chdir("HALL MUSIC") # still have to implement for cd HALL MUSIC/Eprom
+	service.chdir("/")
+	print os.getcwd()
+	service.chdir("..")
+	print os.getcwd()
+	service.chdir("notafolder") # should raise error
+
 service = pfs_service_dropbox()
-
-f = service.open("chop.py", "wr")
-for i in f.readlines():
-	print i
-
-service.close("chop.py")
+# test1(service)
+test2(service)
--- parrot_fs_dropbox.py
+++ parrot_fs_dropbox.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import dropbox
 
 dropbox_authorize_url = 'https://www.dropbox.com/1/oauth2/authorize'
@@ -11,19 +12,38 @@ APP_SECRET = '8fyq6q9qmb0wn6l'
 # ACCESS_TYPE should be 'dropbox' or 'app_folder' as configured for your app
 ACCESS_TYPE = 'app_folder'
 
-# Name of current working directory
-current_dir = '/'
-fd_table = {}
 
 class pfs_file_dropbox:
-	def __init__(self, name, fp, path):
-		self.name = name    # file name
-		self.fp = fp        # file pointer
-		self.path = path    # directory that file was downloaded from
+	def __init__(self, filename, fp, path):
+		self.filename = filename    # file name
+		self.fp = fp                # file pointer
+		self.path = path            # directory that file was downloaded from
 
 class pfs_service_dropbox:
 	def __init__(self):
+
+		# current dir on dropbox
+		self.dir_path = ["/"]
+		self.__update_current_dir()
+
+		# file descriptor table
+		self.fd_table = {}
+
 		self.authorize()
+
+	# PRIVATE METHODS ----------------------------------------------------------#
+	def __update_current_dir(self):
+		self.current_dir = ""
+		for i in self.dir_path:
+			self.current_dir += i
+
+	def __update_os_pwd(self):
+		subprocess.call(["export","PWD=/dropbox" + self.current_dir])
+
+	def __reset_dir_path(self):
+		self.dir_path = ["/"]
+
+	# --------------------------------------------------------------------------#
 
 	def authorize(self):
 		flow = dropbox.client.DropboxOAuth2FlowNoRedirect(APP_KEY, APP_SECRET)
@@ -41,41 +61,78 @@ class pfs_service_dropbox:
 		print access_token
 
 		self.client = dropbox.client.DropboxClient(access_token)
-		print 'linked account: ' + str(self.client.account_info())
+		# print 'linked account: ' + str(self.client.account_info())
 
-	def open(self, name, flags):
-		filename = name.split('/')[-1]
-		path = name.split('/')[:-1]
-		pathstring = ''
-		for i in path:
-			pathstring += i + '/'
-		print pathstring
+	def open(self, filename, flags):
+		old_current = self.current_dir
+		if filename[0] == '/':               # construct directory path list
+			temp_dir_path = ['/']
+		else:
+			temp_dir_path = []
+		for i in filename.split('/')[:-1]:
+			temp_dir_path.append(i)
 
-		current = os.getcwd()
+		# chdir to the directory where the file is located
+		for i in temp_dir_path:
+			self.chdir(i)
+
+		# get the file
+		filename = filename.split('/')[-1]
+
 		os.chdir(DROPBOX_DIR)
-		self.client.get_file(name)
+		try:
+			downloaded_copy = self.client.get_file(filename)
+		except dropbox.rest.ERROR_RESPONSE:
+			raise IOError("[Errno 2] No such file or directory: " + filename)
+
+		local_copy = open(filename, "r+")
+		
+		for i in downloaded_copy.readlines():
+			local_copy.write(i)
+		local_copy.seek(0)
+
+		path = ""
+		for i in temp_dir_path:
+			path += i
+		self.fd_table[filename] = pfs_file_dropbox(filename, local_copy, path)
 
 		print os.getcwd()
-		print name
-		print os.listdir(os.getcwd())
-		
-		fo = open(name, flags)
+		return local_copy
 
-		for i in fo.readlines():
-			print i
+	def close(self, filename):
+		f = self.fd_table[filename]
+		response = self.client.put_file(f.path, f.fp)
+		f.fp.close()
+		del self.fd_table[filename]
 
-		print "---------------------------------------------------"
-		print
-		print
+	def chdir(self, dirname=None):
+		if dirname == "..":
+			if self.current_dir == "/":
+				os.chdir(os.environ["HOME"])
+			else:
+				self.dir_path.pop()
+				self.__update_current_dir()
+				self.__update_os_pwd()
+		elif dirname == ".":
+			pass
+		elif not dirname or dirname == "/":
+			self.__reset_dir_path()
+			self.__update_current_dir()
+			self.__update_os_pwd()
+		else:
+			current_dir_metadata = self.client.metadata(self.current_dir)
+			for i in current_dir_metadata["contents"]:
+				if dirname == i["path"][1:]:
+					if i["is_dir"]:
+						self.dir_path.append(dirname)
+						self.__update_current_dir()
+						self.__update_os_pwd()
+						return
+					else:
+						raise OSError("[Errno 20] Not a directory: " + dirname)
+			raise OSError("[Errno 2] No such file or directory: " + dirname)
 
-		fd_table[name] = pfs_file_dropbox(name, fo, pathstring)
-		os.chdir(current)
-
-		return fo
-
-	def close(self, name):
-		fc = fd_table[name]
-		fc.fp.close()
+						
 
 	# def mkdir(self):
 
